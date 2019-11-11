64d7ce33717414f5f6824a2410a330362e0f64a6
Christopher Sahadeo <christophersahadeo@gmail.com>
2019-11-11 15:24:00 -0400

    create updated

--- create.sql
+++ create.sql
@@ -8,7 +8,16 @@ GO
 USE HRLeave;
 GO
 
--- 'user' is a resered word, so using 'employee' instead
-CREATE TABLE dbo.user (
+CREATE TABLE 
 
+-- 'user' is a resered word, so using 'employee' instead
+CREATE TABLE [dbo].[employee] (
+  [employee_id] NVARCHAR (10) PRIMARY KEY,
+  [ihris_id] NVARCHAR (10) NOT NULL,
+  [first_name] NVARCHAR (30) NOT NULL,
+  [last_name] NVARCHAR (30) NOT NULL,
+  [email] NVARCHAR (60) NOT NULL,
+  -- [auth_id] CHAR (1) NOT NULL, -- not sure about mixed roles implementation
+  [vacation] INT NOT NULL DEFAULT 0,
+  []
 );
