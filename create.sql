2712f11827fe8c8dae589ba294d21a72c04c8947
Christopher Sahadeo <christophersahadeo@gmail.com>
2019-11-12 13:59:18 -0400

    added create and insert queries

--- create.sql
+++ create.sql
@@ -2,22 +2,48 @@ USE master;
 GO
 
 IF DB_ID (N'HRLeave') IS NULL
-CREATE DATABASE HRLeave;
+CREATE DATABASE [HRLeave];
 GO
 
-USE HRLeave;
+USE [HRLeave];
 GO
 
-CREATE TABLE 
+CREATE TABLE [dbo].[authorization] (
+  [auth_id] CHAR (1) PRIMARY KEY, -- might not need the id - just use the auth_name
+  [auth_name] NVARCHAR (15) NOT NULL,
+  [auth_description] NVARCHAR (50)
+);
 
 -- 'user' is a resered word, so using 'employee' instead
 CREATE TABLE [dbo].[employee] (
   [employee_id] NVARCHAR (10) PRIMARY KEY,
   [ihris_id] NVARCHAR (10) NOT NULL,
-  [first_name] NVARCHAR (30) NOT NULL,
+
+  -- active directory info
+  [username] NVARCHAR (60) NOT NULL,
+  [first_name] NVARCHAR (30) NOT NULL, 
   [last_name] NVARCHAR (30) NOT NULL,
   [email] NVARCHAR (60) NOT NULL,
-  -- [auth_id] CHAR (1) NOT NULL, -- not sure about mixed roles implementation
-  [vacation] INT NOT NULL DEFAULT 0,
-  []
+  
+  -- balance per leave type
+  [vacation] INT NOT NULL,
+  [personal] INT NOT NULL,
+  [casual] INT NOT NULL,
+  [sick] INT NOT NULL,
+  [bereavement] INT NOT NULL,
+  [maternity] INT NOT NULL,
+  [pre_retirement] INT NOT NULL
+);
+
+CREATE TABLE [dbo].[employeeauthorization] (
+  [employee_id] NVARCHAR (10),
+  [auth_id] CHAR (1),
+
+  PRIMARY KEY ([employee_id], [auth_id]),
+  FOREIGN KEY ([employee_id])
+    REFERENCES [dbo].[employee] ([employee_id])
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY ([auth_id])
+    REFERENCES [dbo].[authorization] ([auth_id])
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
---
+++ insert.sql
@@ -0,0 +1,7 @@
+USE [HRLeave];
+GO
+
+INSERT INTO [dbo].[employee] ([employee_id], [ihris_id], [username], [first_name], [last_name], [email], [vacation], [personal], [casual], [sick], [bereavement], [maternity], [pre_retirement]) VALUES 
+('1', '1', 'PLANNING\ Tristan Sankar', 'Tristan', 'Sankar', 'tristan.sankar@planning.gov.tt', 0, 5, 0, 14, 2, 0, 0);
+
+
