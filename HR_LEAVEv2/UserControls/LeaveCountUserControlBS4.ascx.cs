26d3e183ad7fa04c25adb99781c42373b19f35e3
Tristan Sankar <Tristan.Sankar@planning.gov.tt>
2020-01-13 15:23:08 -0400

    Leave balance user control displays appropriate leave balances based on whether contract and whether they are in a starting period of their contract or otherwise. Includes for public service.

--- HR_LEAVEv2/UserControls/LeaveCountUserControlBS4.ascx.cs
+++ HR_LEAVEv2/UserControls/LeaveCountUserControlBS4.ascx.cs
@@ -13,35 +13,161 @@ namespace HR_LEAVEv2.UserControls
     public partial class LeaveCountUserControlBS4 : System.Web.UI.UserControl
     {
 
+        private class EmployeeDetails
+        {
+            public string sick { get; set; }
+            public string vacation { get; set; }
+            public string personal { get; set; }
+            public string casual { get; set; }
+            public string employmentType { get; set; }
+            public string start_date { get; set; }
+        };
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
+                // get employee employment type
+                //if contract
+                //  display sick leave balance
+                //  display personal leave balance if in first 11 months of contract
+                //  display vacation leave balance if in 12th month+ of contract
+
+                //if public service
+                //  display sick leave balance
+                //  display casual leave balance
+
+
+                vacationPanel.Visible = false;
+                personalPanel.Visible = false;
+                casualPanel.Visible = false;
+                sickPanel.Visible = true;
+
+                EmployeeDetails empDetails = null;
                 string CS = ConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString;
                 using (SqlConnection con = new SqlConnection(CS))
                 {
-                    SqlCommand cmd = new SqlCommand(
-                        $@"
-                            SELECT 
-                                [sick], [vacation], [personal]
-                            FROM 
-                                [dbo].[employee]
-                            WHERE
-                                [employee_id] = '{Session["emp_id"]}'
+                    string getEmpDetailsSql = $@"
+                            SELECT e.[sick], e.[vacation], e.[personal], e.[casual], ep.employment_type, FORMAT(ep.start_date, 'MM/dd/yy') as start_date
+                            FROM [dbo].[employee] e
+                            LEFT JOIN [dbo].employeeposition ep
+                            ON e.employee_id = ep.employee_id AND GETDATE()>=ep.start_date AND GETDATE()<=ep.expected_end_date
+                            WHERE e.[employee_id] = '{Session["emp_id"]}'
                             ;
-                        ", con);
-                    con.Open();
-                    SqlDataReader rdr = cmd.ExecuteReader();
-
-                    if (rdr.Read())
+                        ";
+                    using (SqlCommand cmd = new SqlCommand(getEmpDetailsSql, con))
                     {
-                        //h2Sick.Attributes["data-to"] = rdr["sick"].ToString();
-                        //h2Vacation.Attributes["data-to"] = rdr["vacation"].ToString();
-                        //h2Personal.Attributes["data-to"] = rdr["personal"].ToString();
-                        ViewState["sick"] = rdr["sick"].ToString();
-                        ViewState["vacation"] = rdr["vacation"].ToString();
-                        ViewState["personal"] = rdr["personal"].ToString();
+                        con.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+
+                                empDetails = new EmployeeDetails
+                                {
+                                    sick = reader["sick"].ToString(),
+                                    vacation = reader["vacation"].ToString(),
+                                    personal = reader["personal"].ToString(),
+                                    casual = reader["casual"].ToString(),
+                                    employmentType = reader["employment_type"].ToString(),
+                                    start_date = reader["start_date"].ToString(),
+                                };
+                            }
+                        }     
                     }
+                        
+
+                    ViewState["sick"] = empDetails.sick;
+                    ViewState["vacation"] = empDetails.vacation;
+                    ViewState["personal"] = empDetails.personal;
+                    ViewState["casual"] = empDetails.casual;
+
+                    
+
+                    if(empDetails.employmentType == "Contract")
+                    {
+                        /* contract employees
+                         * for a new employee: 
+                         *  vacation = 20 
+                         *  personal = 5
+                         * 
+                         * During the first 11 months of contract, employee only has access to the 5 personal days. 
+                         * After the 11 months, they have access to their:
+                         *          vacation days - days used from personal
+                        */
+
+                        // is employee in first 11 months of contract?
+                        DateTime startDate = Convert.ToDateTime(empDetails.start_date);
+                        DateTime elevenMonthsFromStartDate = startDate.AddMonths(11);
+
+                        if (DateTime.Compare(DateTime.Today, elevenMonthsFromStartDate) < 0)
+                            // display personal 
+                            personalPanel.Visible = true;
+                        else
+                        {
+                            int personal = Convert.ToInt16(empDetails.personal);
+
+                            if(personal > 0)
+                            {
+                                // subtract personal days used from vacation
+                                int updatedVacation = Convert.ToInt16(empDetails.vacation);
+                                updatedVacation = updatedVacation - (5 - personal);
+
+                                if(updatedVacation != Convert.ToInt16(empDetails.vacation))
+                                {
+                                    // update vacation and personal in db
+                                    try
+                                    {
+                                       string updateVacationSql = $@"
+                                            BEGIN TRANSACTION [updateVacation]
+                                            
+
+                                            BEGIN TRY
+
+                                                UPDATE [dbo].employee 
+                                                SET vacation = '{updatedVacation}'
+                                                WHERE employee_id = '{Session["emp_id"]}';
+
+                                                UPDATE [dbo].employee 
+                                                SET personal = '0'
+                                                WHERE employee_id = '{Session["emp_id"]}';
+
+                                                COMMIT TRANSACTION [updateVacation]
+
+                                              END TRY
+
+                                              BEGIN CATCH
+
+                                                  ROLLBACK TRANSACTION [updateVacation]
+
+                                              END CATCH  
+                                        ";
+                                        using (SqlCommand cmd = new SqlCommand(updateVacationSql, con))
+                                        {
+                                            int rowsAffected = cmd.ExecuteNonQuery();
+                                            if(rowsAffected > 0) { 
+                                                ViewState["vacation"] = updatedVacation.ToString();
+                                            }
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        //exception logic
+                                        Console.WriteLine(ex.Message.ToString());
+                                    }
+                                }
+                            }
+
+                            // display vacation 
+                            vacationPanel.Visible = true;
+                        }
+                            
+                    } else
+                    {
+                        // public service employees
+                        casualPanel.Visible = true;
+                        vacationPanel.Visible = true;
+                    }
+
                 }
             }
         }
--- HR_LEAVEv2/UserControls/LeaveCountUserControlBS4.ascx.designer.cs
+++ HR_LEAVEv2/UserControls/LeaveCountUserControlBS4.ascx.designer.cs
@@ -11,5 +11,41 @@ namespace HR_LEAVEv2.UserControls {
     
     
     public partial class LeaveCountUserControlBS4 {
+        
+        /// <summary>
+        /// sickPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel sickPanel;
+        
+        /// <summary>
+        /// vacationPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel vacationPanel;
+        
+        /// <summary>
+        /// personalPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel personalPanel;
+        
+        /// <summary>
+        /// casualPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel casualPanel;
     }
 }
