93705b545763317a9e070a3c48ee85935426065d
Tristan Sankar <Tristan.Sankar@planning.gov.tt>
2020-05-21 15:05:57 -0400

    added page to upload excel

---
+++ HR_LEAVEv2/HR/UploadData.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UploadData.aspx.cs" Inherits="HR_LEAVEv2.HR.UploadData" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1><%: Title %></h1>
+    <div class="container-fluid" style="margin-top:35px;">
+        <asp:Panel ID="fileUploadPanel" runat="server" Style="margin: 0 auto; text-align: center" CssClass="row">
+            <label for="FileUpload1" style="font-size: 1.2em; display: inline;">Upload Files:</label>
+            <asp:FileUpload ID="FileUpload1" runat="server" Width="475px" Style="margin: 0 auto; display: inline-block; background-color: lightgrey" AllowMultiple="false" />
+            <asp:LinkButton ID="uploadFilesBtn" runat="server" OnClick="uploadFilesBtn_Click" CssClass="btn btn-sm btn-primary content-tooltipped" data-toggle="tooltip" data-placement="top" title="Upload file">
+                <i class="fa fa-upload" aria-hidden="true"></i>
+            </asp:LinkButton>
+            <asp:LinkButton ID="clearFileBtn" runat="server" OnClick="clearFileBtn_Click" OnClientClick="return confirm('Clear uploaded file?');" CssClass="btn btn-sm btn-danger content-tooltipped" data-toggle="tooltip" data-placement="top" title="Clear uploaded file">
+                <i class="fa fa-times" aria-hidden="true"></i>
+            </asp:LinkButton>
+            <br />
+            <asp:Label ID="uploadedFile" runat="server" Text="" Visible ="false"></asp:Label>
+        </asp:Panel>
+        <asp:Panel ID="chooseTablePanel" runat="server" Style="display:flex; justify-content:center; margin-top:30px;" Visible="false">
+<%--            <asp:Label ID="Label1" runat="server" Text="Choose a table to upload data to:"></asp:Label>--%>
+            <asp:DropDownList ID="tableSelectDdl" runat="server" AutoPostBack="true" OnSelectedIndexChanged="tableSelectDdl_SelectedIndexChanged">
+                <asp:ListItem Value="-">Choose a Table</asp:ListItem>
+                <asp:ListItem Value="assignment">Assignment</asp:ListItem>
+                <asp:ListItem Value="department">Department</asp:ListItem>
+                <asp:ListItem Value="employee">Employee</asp:ListItem>
+                <asp:ListItem Value="employeeposition">EmployeePosition</asp:ListItem>
+                <asp:ListItem Value="employmenttype">EmploymentType</asp:ListItem>
+                <asp:ListItem Value="leavetype">LeaveType</asp:ListItem>
+                <asp:ListItem Value="position">Position</asp:ListItem>
+            </asp:DropDownList>
+        </asp:Panel>
+        <asp:Panel ID="uploadDataBtnPanel" runat="server" Visible=" false" Style="text-align:center; margin-top:15px;">
+            <asp:LinkButton ID="uploadDataBtn" runat="server" CssClass="btn btn-primary" OnClick="uploadDataBtn_Click">
+                <i class="fa fa-upload navbar-link-icon"></i>
+                Upload data
+            </asp:LinkButton>
+        </asp:Panel>
+        <asp:UpdatePanel ID="applyModeFeedbackUpdatePanel" runat="server" Style="margin-top:15px;; text-align:center;">
+            <ContentTemplate>
+                <asp:Panel ID="invalidFileTypePanel" runat="server" CssClass="row alert alert-warning" Style="display: none; margin: 0px 5px;" role="alert">
+                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                    <span>Invalid file type</span>
+                </asp:Panel>
+
+                <asp:Panel ID="fileUploadedTooLargePanel" runat="server" CssClass="row alert alert-warning" Style="display: none; margin: 0px 5px;" role="alert">
+                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                    <span>File upload too large</span>
+                </asp:Panel>
+
+                <asp:Panel ID="noFileUploaded" runat="server" CssClass="row alert alert-warning" Style="display: none; margin: 0px 5px;" role="alert">
+                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                    <span >No file was uploaded</span>
+                </asp:Panel>
+
+                <asp:Panel ID="successfulDataInsertPanel" runat="server" CssClass="row alert alert-success" Style="display: none; margin: 0px 5px;" role="alert">
+                    <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+                    <span >Data was successfully uploaded to database</span>
+                </asp:Panel>
+
+                <asp:Panel ID="unsuccessfulInsertPanel" runat="server" CssClass="row alert alert-danger" Style="display: none; margin: 0px 5px;" role="alert">
+                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+                    <span>Data was not uploaded to database</span>
+                </asp:Panel>
+
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+</asp:Content>
---
+++ HR_LEAVEv2/HR/UploadData.aspx.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using ClosedXML.Excel;
+using System.IO;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using HR_LEAVEv2.Classes;
+
+namespace HR_LEAVEv2.HR
+{
+    public partial class UploadData : System.Web.UI.Page
+    {
+        Util util = new Util();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> permissions = (List<string>)Session["permissions"];
+            if (permissions == null || !(permissions.Contains("hr1_permissions") || permissions.Contains("hr2_permissions") || permissions.Contains("hr3_permissions")))
+                Response.Redirect("~/AccessDenied.aspx");
+
+            // hide clear file button if no file is uploaded
+            clearFileBtn.Visible = Session["uploadedFile"] != null;
+        }
+
+        protected void resetUploadDataPage()
+        {
+            invalidFileTypePanel.Style.Add("display", "none");
+            fileUploadedTooLargePanel.Style.Add("display", "none");
+            noFileUploaded.Style.Add("display", "none");
+            successfulDataInsertPanel.Style.Add("display", "none");
+            unsuccessfulInsertPanel.Style.Add("display", "none");
+            uploadedFile.Visible = false;
+            uploadDataBtnPanel.Visible = false;
+            chooseTablePanel.Visible = false;
+            tableSelectDdl.SelectedIndex = 0;
+        }
+
+        protected void uploadFilesBtn_Click(object sender, EventArgs e)
+        {
+            resetUploadDataPage();
+
+            if (FileUpload1.HasFile)
+            {
+                Boolean isInvalidFileType = false,
+                        isFileTooLarge = false;
+
+                // used to check whether the files uploaded fit the size requirement specified in the web config
+                HttpRuntimeSection section = ConfigurationManager.GetSection("system.web/httpRuntime") as HttpRuntimeSection;
+                int maxRequestLength = section != null ? section.MaxRequestLength : 4096;
+
+                // used to check whether the file(s) uploaded are of a certain format
+                List<string> allowedFileExtensions = new List<string>() { ".xlsx" };
+                HttpPostedFile fileToBeUploaded = FileUpload1.PostedFile;
+
+                isFileTooLarge = fileToBeUploaded.ContentLength > maxRequestLength;
+                isInvalidFileType = !allowedFileExtensions.Contains(Path.GetExtension(fileToBeUploaded.FileName).ToString());
+
+                if (isInvalidFileType)
+                {
+                    invalidFileTypePanel.Style.Add("display", "inline-block");
+                }
+
+                if (isFileTooLarge)
+                    fileUploadedTooLargePanel.Style.Add("display", "inline-block");
+
+                if (!isInvalidFileType && !isFileTooLarge)
+                {
+                    // add files to session so they will persist after postback
+                    Session["uploadedFile"] = Path.GetFileName(fileToBeUploaded.FileName);
+                    FileUpload1.SaveAs(Path.Combine(Server.MapPath("~/Assets/temp"), FileUpload1.FileName));
+                    uploadedFile.Visible = true;
+                    uploadedFile.Text = $"Uploaded file: <strong>{Path.GetFileName(fileToBeUploaded.FileName)}</strong>";
+                    chooseTablePanel.Visible = true;
+                    clearFileBtn.Visible = true;
+                }
+            }
+            else
+            {
+                noFileUploaded.Style.Add("display", "inline-block");
+            }
+        }
+
+        protected void deleteUploadedFile()
+        {
+            resetUploadDataPage();
+            if (Session["uploadedFile"] != null && File.Exists(Path.Combine(Server.MapPath("~/Assets/temp"), Session["uploadedFile"].ToString())))
+                File.Delete(Path.Combine(Server.MapPath("~/Assets/temp"), Session["uploadedFile"].ToString()));
+            Session["uploadedFile"] = null;
+            FileUpload1.Dispose();
+        }
+
+        protected void clearFileBtn_Click(object sender, EventArgs e)
+        {
+            deleteUploadedFile();
+        }
+
+        protected DataTable getDataTableRepresentingTable(string tableName)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString))
+                {
+                    connection.Open();
+                    string sql = $@"
+                            SELECT *
+                            FROM dbo.{tableName}
+                            WHERE 1=2
+                        ";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+
+                            return dt;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //exception logic
+                return null;
+            }
+
+        }
+
+        protected void uploadDataBtn_Click(object sender, EventArgs e)
+        {
+            Boolean isUploadSuccessful = false;
+            if (Session["uploadedFile"] != null)
+            {
+                string filePath = Path.Combine(Server.MapPath("~/Assets/temp"), Session["uploadedFile"].ToString());
+                if (File.Exists(filePath))
+                {
+                    //Create a new DataTable.
+                    DataTable dt = getDataTableRepresentingTable(tableSelectDdl.SelectedValue);
+                    //DataTable dt = new DataTable();
+                    //dt.Columns.Add("test_col1", typeof(int));
+                    //dt.Columns.Add("test_col2", typeof(string));
+
+                    // Excel file cannot be open when trying to read it
+                    try
+                    {
+                        using (XLWorkbook workBook = new XLWorkbook(filePath))
+                        {
+                            //Read the first Sheet from Excel file.
+                            IXLWorksheet workSheet = workBook.Worksheet(1);
+
+                            bool firstRow = true;
+                            foreach (IXLRow row in workSheet.Rows())
+                            {
+                                if (!row.IsEmpty())
+                                {
+                                    //Use the first row to add columns to DataTable.
+                                    if (firstRow)
+                                    {
+                                        firstRow = false;
+
+                                    }                                       
+                                    else
+                                    {
+                                        //Add rows to DataTable.
+                                        dt.Rows.Add();
+                                        int i = 0;
+
+                                        foreach (IXLCell cell in row.Cells(1, dt.Columns.Count))
+                                        {
+                                            //if (!String.IsNullOrEmpty(cell.Value.ToString()) && !String.IsNullOrWhiteSpace(cell.Value.ToString()))
+                                            //{
+                                                dt.Rows[dt.Rows.Count - 1][i] = cell.Value.ToString();
+                                                i++;
+                                            //}
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+
+                        // asks the question: are there more rows than just the header row?
+                        isUploadSuccessful = dt.Rows.Count > 1;
+                    }
+                    catch (Exception exc)
+                    {
+                        isUploadSuccessful = false;
+                    }
+
+
+                    // upload to db
+                    try
+                    {
+                        using (var bulkCopy = new SqlBulkCopy(ConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString, SqlBulkCopyOptions.KeepIdentity))
+                        {
+                            foreach (DataColumn col in dt.Columns)
+                            {
+                                bulkCopy.ColumnMappings.Add(col.ColumnName, col.ColumnName);
+                            }
+
+                            bulkCopy.BulkCopyTimeout = 600;
+                            bulkCopy.DestinationTableName = tableSelectDdl.SelectedValue;
+                            bulkCopy.WriteToServer(dt);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //exception logic
+                        isUploadSuccessful = false;
+                    }
+                }
+                
+
+            }
+
+            if (isUploadSuccessful)
+            {
+                // delete file from temp and show success message
+                deleteUploadedFile();
+                successfulDataInsertPanel.Style.Add("display", "inline-block");
+            } else 
+                unsuccessfulInsertPanel.Style.Add("display", "inline-block");
+        }   
+
+        protected void tableSelectDdl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            uploadDataBtnPanel.Visible = tableSelectDdl.SelectedValue != "-";
+        }
+    }
+}
